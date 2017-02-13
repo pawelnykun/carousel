1015a3a10aabdde74c5fa752b040fc84e39d50d0
pawelnykun <pawel.nykun@gmail.com>
2017-02-13 17:09:17 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,41 @@
 $(function() {
-	var carouselList = $("#carousel ul");
+	//declaration and initialization of the variables
+	var carousel = $("#carousel");
+	var carouselList = carousel.find("ul.photo");
+	var carouselMenu = carousel.find("ul.menu");
+	//adding <li> elements depending on the number of photos
+	carouselList.find("li").each(function() {
+		carouselMenu.append("<li></li>");
+	});
 
-	function moveFirstSlide() {
-		var firstItem = carouselList.find("li:first");
-		var lastItem = carouselList.find("li:last");
-		lastItem.after(firstItem);
-		carouselList.css({marginLeft:0});
+	var menuOption = carouselMenu.find("li");
+	//adding class onload
+	menuOption.first().addClass("active");
+	//ater click function
+	menuOption.click(function() {
+		if( !$(this).hasClass("active") ) {
+			target = $(this).index();
+			moveSliderTo(target);
+		}
+	});
+
+	function autoChangeSlide() {
+		target = menuOption.siblings(".active").index();
+        target == menuOption.length - 1 ? target = 0 : target += 1;
+        moveSliderTo(target);
 	}
 
-	function changeSlide() {
-		carouselList.animate({'marginLeft':-470}, 500, moveFirstSlide);
+	function moveSliderTo(target) {
+		carouselList.stop().animate({"left": -470 * target});
+        menuOption.removeClass("active").eq(target).addClass("active");
+        resetInterval();
+	}
+	//setting interval
+	var autoMove = setInterval(function(){autoChangeSlide();}, 3000);
+
+	function resetInterval() {
+		clearInterval(autoMove);
+		autoMove = setInterval(function(){autoChangeSlide();}, 3000);
 	}
 
-	setInterval(changeSlide, 3000);
 });
