ca4eaa8f13cffc22a54e51914f3a90c828b063cb
pawelnykun <pawel.nykun@gmail.com>
2017-02-15 19:48:03 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -18,13 +18,25 @@ $(function() {
 			moveSliderTo(target);
 		}
 	});
+	//prev and next buttons
+	carousel.find(".prev").click(function() {
+		target = menuOption.siblings(".active").index();
+		target == 0 ? target = menuOption.length - 1 : target -= 1;
+        moveSliderTo(target);
+	})
 
+	carousel.find(".next").click(function() {
+		target = menuOption.siblings(".active").index();
+		target == menuOption.length - 1 ? target = 0 : target += 1;
+		moveSliderTo(target);
+	})
+	//automoving
 	function autoChangeSlide() {
 		target = menuOption.siblings(".active").index();
         target == menuOption.length - 1 ? target = 0 : target += 1;
         moveSliderTo(target);
 	}
-
+	//moving to the selected slide
 	function moveSliderTo(target) {
 		carouselList.stop().animate({"left": -470 * target});
         menuOption.removeClass("active").eq(target).addClass("active");
@@ -32,10 +44,9 @@ $(function() {
 	}
 	//setting interval
 	var autoMove = setInterval(function(){autoChangeSlide();}, 3000);
-
+	//reseting interval
 	function resetInterval() {
 		clearInterval(autoMove);
 		autoMove = setInterval(function(){autoChangeSlide();}, 3000);
 	}
-
 });
